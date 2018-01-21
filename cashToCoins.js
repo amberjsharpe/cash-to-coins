9ceca1b1ee720be7a49d92b05837cbfc347e2b9a
Amber Sharpe <amberjsharpe@gmail.com>
2018-01-21 16:28:50 -0600

    can change one cash value into coin

---
+++ cashToCoins.js
@@ -0,0 +1,19 @@
+// Create coins in object
+
+var piggyBank = {quarters: 0, dimes: 0, nickels: 0, pennies: 0};
+var dollarAmount = .25;
+
+//Convert specific cash value to coin
+
+    if (dollarAmount = .25) {
+        piggyBank.quarters += 1
+    } else if (dollarAmount = .10) {
+        piggyBank.dimes += 1
+    } else if (dollarAmount = .05) {
+        piggyBank.nickels += 1
+    } else if (dollarAmount = .01) {
+        piggyBank.pennies += 1
+    }
+
+
+console.log(piggyBank);
