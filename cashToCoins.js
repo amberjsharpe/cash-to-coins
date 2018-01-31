582508844dca2a180647648a64bf0dbc16662f83
Amber Sharpe <amberjsharpe@gmail.com>
2018-01-30 20:07:05 -0600

    Misread instructions - JS works now

--- cashToCoins.js
+++ cashToCoins.js
@@ -1,19 +1,23 @@
-// Create coins in object
+// Put coins in your piggy bank
 
-var piggyBank = {quarters: 0, dimes: 0, nickels: 0, pennies: 0};
-var dollarAmount = .25;
+const piggyBank = {quarters: 26, dimes: 39, nickels: 5, pennies: 7};
 
-//Convert specific cash value to coin
+// What is the value of each
 
-    if (dollarAmount = .25) {
-        piggyBank.quarters += 1
-    } else if (dollarAmount = .10) {
-        piggyBank.dimes += 1
-    } else if (dollarAmount = .05) {
-        piggyBank.nickels += 1
-    } else if (dollarAmount = .01) {
-        piggyBank.pennies += 1
-    }
+const coinValue = {quarters: .25, dimes: .10, nickels: .05, pennies: .01};
+
+// Get coin amounts with math
+let quartersValue = piggyBank.quarters * coinValue.quarters;
+let dimesValue = piggyBank.dimes * coinValue.dimes;
+let nickelsValue = piggyBank.nickels * coinValue.nickels;
+let penniesValue = piggyBank.pennies * coinValue.pennies;
+
+// Add coin amounts together
+
+var piggyCash = quartersValue + dimesValue + nickelsValue + penniesValue;
+
+// Make it rain
+console.log(piggyCash);
 
 
-console.log(piggyBank);
+
